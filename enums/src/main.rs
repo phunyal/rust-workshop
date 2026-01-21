9e36c24c14d65486ab5d9fc9fdac4f5a269deb13
Prashish Phunyal <prolwalin@gmail.com>
2026-01-21 09:50:34 +0000

    Add implementation of Coin enum and value_in_cents function with main functionality

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,13 +1,38 @@
-enum IpAddrKind {
-    V4(u8, u8, u8, u8),
-    V6(String),
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... other states can be added here
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
 }
 
 fn main() {
-    let home = IpAddrKind::V4(127, 0, 0, 1));
 
-    let loopback = IpAddrKind::V6(String::from("::1"));
-
-    println!("{home}");
-    println!("{loopback}");
+    println!("{:?}", Coin::Dime);
+    println!("Value of a dime: {} cents", value_in_cents(Coin::Dime));
+    println!("Value of a quarter: {} cents", value_in_cents(Coin::Quarter));
+    println!("Value of a nickel: {} cents", value_in_cents(Coin::Nickel));
+    println!("Value of a penny: {} cents", value_in_cents(Coin::Penny));
+    Coin::Quarter(String::from("Nepal"));
 }
